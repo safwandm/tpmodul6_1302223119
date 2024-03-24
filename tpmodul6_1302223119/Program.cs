a8aae219f4180f01597d9e4eed3ca3bed4fce450
safwandm <safwan.damanik@gmail.com>
2024-03-25 00:47:41 +0700

    implementasi design contract

--- tpmodul6_1302223119/Program.cs
+++ tpmodul6_1302223119/Program.cs
@@ -5,13 +5,15 @@ using System.Diagnostics.Contracts;
 using tpmodul6_1302223119;
 
 SayaTubeVideo vid1 = new SayaTubeVideo("Tutorial Design By Contract - [MuhammadFaisalShafwanDamanik]");
+vid1.IncreasePlayCount(10000000);
+vid1.PrintVideoDetails();
+
 SayaTubeVideo error1 = new SayaTubeVideo("");
 SayaTubeVideo error2 = new SayaTubeVideo("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
-
-vid1.IncreasePlayCount(10000001);
+error1.IncreasePlayCount(10000001);
 
 for (int i = 0; i < 215; ++i)
 {
-    vid1.IncreasePlayCount(10000000);
+    error2.IncreasePlayCount(10000000);
 }
